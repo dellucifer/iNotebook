30aaa432f79663af62fa34b3aac793673b29d1ef
Dellucifer <pg99285@gmail.com>
2022-08-09 23:56:25 +0530

    Updated errors and connections

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
-//Creating a user using: POST "/api/auth/"
+//Creating a user using: POST "/api/auth/createuser" No login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Username must be of atleast 5 characters").isLength({
       min: 3,
@@ -15,22 +15,30 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    //If error exists, then return bad request and error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-    User.create({
+    
+    //check whether the user with same email exists or not
+    try {
+        let user = await User.findOne({email: req.body.email})
+    if(user){
+        return res.status(400).json({error: "Email already exists"})
+    }
+    user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
     })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "Email already in use", message: err.message });
-      });
+    res.json(user)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal Server Error")
+    }
+    
   }
 );
 
