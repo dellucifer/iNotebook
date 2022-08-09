aa7ebb4a9539eb79e992328439293b1b9eb6fa8b
Dellucifer <pg99285@gmail.com>
2022-08-10 01:20:49 +0530

    Created Login Mechanism

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -38,8 +38,8 @@ router.post(
     //Create a new user
     user = await User.create({
       name: req.body.name,
-      email: secPass,
-      password: req.body.password,
+      email: req.body.email,
+      password: secPass,
     })
     const data = {
         user: {
@@ -56,4 +56,39 @@ router.post(
   }
 );
 
+//Authenticating a user using: POST "/api/auth/login" No login required
+router.post(
+    "/login",
+    [
+      body("email", "Enter a valid email").isEmail(),
+      body("password", "Password cannot be blank").exists()
+    ], async (req, res) => {
+        //If error exists, then return bad request and error
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {email, password} = req.body
+        try {
+            let user = await User.findOne({email})
+            if(!user){
+                return res.status(400).json({error: "Invalid Credentials"})
+            }
+            const passwordCompare = await bcrypt.compare(password, user.password)
+            if(!passwordCompare){
+                return res.status(400).json({error: "Invalid Credentials"})
+            }
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SEC)
+            res.json({authToken})
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("Internal Server Error")
+        }
+    })
 module.exports = router;
