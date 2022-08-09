7bf8099e603c9cfa0d5603b400f03dcb7d21686d
Dellucifer <pg99285@gmail.com>
2022-08-09 16:27:52 +0530

    auth validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,4 +21,6 @@ const userSchema = new Schema({
     }
   });
 
-  module.exports = mongoose.model('user', userSchema);
+  const User = mongoose.model('user', userSchema);
+  User.createIndexes();
+  module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,37 @@
-const express = require('express');
-const User = require('../models/User');
-const router = express.Router()
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 //Creating a user using: POST "/api/auth/"
-router.post('/', (req, res)=>{
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
-})
+router.post(
+  "/",
+  [
+    body("name", "Username must be of atleast 5 characters").isLength({
+      min: 3,
+    }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be of atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports = router 
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({ error: "Email already in use", message: err.message });
+      });
+  }
+);
+
+module.exports = router;
