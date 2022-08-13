9df16332f8b128cb76dee6baec7e56a514b52f2d
Dellucifer <pg99285@gmail.com>
2022-08-13 23:05:08 +0530

    Router Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from './Components/About'
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/about' element={<About/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
